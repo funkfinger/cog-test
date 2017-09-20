670a527a4211c948863e929951ed05a42e0033bf
Jay Wiggins <jay@irk.com>
2017-09-19 19:54:00 -0700

    reset

--- app/routes/signin.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET signin page. */
-router.get('/', function(req, res, next) {
-  res.render('signin', { title: 'Sign In With Amazon' });
-});
-
-module.exports = router;
